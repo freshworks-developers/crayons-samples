ec308bc6277f6a1531da0490711cec027b03a660
Saif Ali Shaik <saifshine7@gmail.com>
2022-04-10 16:27:10 +0530

    📦 NEW: refactor app.js

--- freshdesk/app/scripts/app.js
+++ freshdesk/app/scripts/app.js
@@ -1,49 +1,34 @@
-document.addEventListener("DOMContentLoaded", function () {
-  app.initialized()
-    .then(function (_client) {
-      window.client = _client;
-      client.events.on('app.activated', function () {
-        onLoadClickEventHandler();
-      });
-    })
-    .catch(function (error) {
-      console.error(error);
-      showNotification('danger', 'Sorry! Unable to load app');
-    });
-});
+var client;
 
-/**
- *   Register click event handler for `Create Ticket` button
- */
-function onLoadClickEventHandler() {
-  document.getElementById('createTicket').addEventListener('click', function () {
-    // Ticket title fetched from user Input
+init();
+
+async function init() {
+  client = await app.initialized();
+  client.events.on('app.activated', loadValidationScript);
+}
+
+async function loadValidationScript() {
+  document.getElementById('clearDetails').addEventListener('click', clearInputfields);
+  document.getElementById('createTicket').addEventListener('click', async function validateFields() {
     var title = document.getElementById('title').value;
-    // Description of the ticket fetched from user input
     var desc = document.getElementById('desc').value;
-    // Email id of the user, creating the ticket
     var email = document.getElementById('email').value;
+
     if (title && desc && email) {
-      createFreshdeskTicket(title, desc, email);
+      await createFreshdeskTicket(title, desc, email);
     } else {
-      showNotification('danger', 'Ticket Values cannot empty, Fill all values');
+      await showNotification('danger', 'Ticket Values cannot empty, Fill all values');
     }
   });
 }
 
-// 1.2 Paste the code for createFreshdeskTicket() here 👇🏼 !
-/**
-* 1.2 Function to create a Freshdesk ticket ! 🤘
-* @param {String} title          Ticket title
-* @param {String} description    Ticket description
-* @param {String} email          email of the user that creates ticket
-*/
-function createFreshdeskTicket(title, description, email) {
-  client.iparams.get("freshdesk_subdomain").then(function (iparam) {
-    client.request.post(`https://${iparam.freshdesk_subdomain}.freshdesk.com/api/v2/tickets`, {
+async function createFreshdeskTicket(title, description, email) {
+  try {
+    let iparam = await client.iparams.get('freshdesk_subdomain');
+    await client.request.post(`https://${iparam.freshdesk_subdomain}.freshdesk.com/api/v2/tickets`, {
       headers: {
-        Authorization: "Basic <%= encode(iparam.freshdesk_api_key)%>",
-        "Content-Type": "application/json;charset=utf-8"
+        Authorization: 'Basic <%= encode(iparam.freshdesk_api_key)%>',
+        'Content-Type': 'application/json;charset=utf-8'
       },
       body: JSON.stringify({
         description: `${description}`,
@@ -52,38 +37,21 @@ function createFreshdeskTicket(title, description, email) {
         status: 2,
         subject: `${title}`
       })
-    })
-      .then(function () {
-        showNotification('success', 'Ticket is successfully created');
-        //Clears user input after posting data
-        clearInputfields();
-      })
-      .catch(function (error) {
-        console.error(error);
-        showNotification('danger', 'Unable to create ticket');
-      });
-  })
-    .catch(function (error) {
-      console.error(error);
-      showNotification('danger', 'Unable to get iparams');
     });
+    await showNotification('success', 'Ticket is successfully created');
+  } catch (error) {
+    console.error(error);
+    await showNotification('danger', 'Unable to create ticket');
+  }
 }
 
-/**
- * Function to show notifications to the user
- * @param {String} status   	Status of the notification
- * @param {String} message  	Custom notification message
- */
-function showNotification(status, message) {
-  client.interface.trigger("showNotify", {
+async function showNotification(status, message) {
+  client.interface.trigger('showNotify', {
     type: `${status}`,
     message: `${message}`
   });
 }
 
-/**
- * Clear the input fields
- */
 function clearInputfields() {
   document.getElementById('title').value = '';
   document.getElementById('desc').value = '';
